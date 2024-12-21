ea206bb75f538574a1dd6ba74f413cf41ac6b54b
Joan Frago <jfragojimenez@gmail.com>
2024-12-21 19:35:04 +0100

    Use a virtual environment to ensure it uses python3.10 and add paragraphs to api outputs

--- stjaume/api/index.php
+++ stjaume/api/index.php
@@ -2,7 +2,7 @@
 
 function EstatCaldera() {
     // Executes the Python script to get the boiler status
-    $estatCaldera = trim(shell_exec("python3 ../code/pyt/estatCaldera.py")); 
+    $estatCaldera = trim(shell_exec("/home/joan/venv3.10/bin/python ../code/pyt/estatCaldera.py")); 
     return $estatCaldera; // Return the value instead of echoing
 }
 
@@ -11,18 +11,22 @@ function ControlCaldera() {
     $estatCaldera = EstatCaldera();
 
     if ($estatCaldera === "ON") {
-        return "La caldera està encesa";
+        return "<p>La caldera ja està encesa</p>";
     } elseif ($estatCaldera === "OFF") {
-        // Only execute encendreCaldera.py if it's off
-        $output = shell_exec("python3 ../code/pyt/encendreCaldera.py");
-        return $output ? $output : "Error al encendre la caldera";
+	// Only execute encendreCaldera.py if it's off
+	$output = "<p>Running in real mode.</p>";
+        $scriptOutput = shell_exec("/home/joan/venv3.10/bin/python ../code/pyt/encendreCaldera.py");
+        $output .= $scriptOutput ? "<p>" . htmlspecialchars($scriptOutput) . "</p>" : "Error al encendre la caldera";
+	return $output;
     }
     // This else should only be used to test, and ensure it is working
     // Without it, when running off of the server it won't do anything because $estatCaldera will
     // be an error, not ON or OFF
     else {
-        $output = shell_exec("python3 ../code/pyt/encendreCaldera.py");
-        return $output ? $output : "Error al encendre la caldera";
+	$output = "<p>Running in test mode.</p>";
+        $scriptOutput = shell_exec("/home/joan/venv3.10/bin/python ../code/pyt/encendreCaldera.py");
+	$output .= $scriptOutput ? "<p>" .htmlspecialchars($scriptOutput) . "</p>" : "Error al encendre la caldera";
+	return $output;
     }
 }
 
